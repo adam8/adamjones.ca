4fd4ce30f6a713b53d1894bfc55bec139ef34c38
Adam Jones <adamjones.ca@gmail.com>
2021-03-14 22:15:48 -0700

    clean up, fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders name', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/adam skye jones/i);
   expect(linkElement).toBeInTheDocument();
 });
